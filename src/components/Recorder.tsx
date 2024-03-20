14170da8cb8cd52e7caa43317f160ea879848dca
Márk Sági <sagi.mark.13@gmail.com>
2024-03-20 16:41:28 +0100

    ez az uj

--- src/components/Recorder.tsx
+++ src/components/Recorder.tsx
@@ -1,36 +1,112 @@
-import { useEffect } from 'react';
-import { useAudioRecorder } from 'react-audio-voice-recorder';
-import { Button } from 'react-bootstrap';
-  
-  export default function Recorder ()
-  {
-    const {
-      startRecording,
-      stopRecording,
-      //togglePauseResume,
-      recordingBlob,
-      isRecording,
-      //isPaused,
-      //recordingTime,
-      //mediaRecorder
-    } = useAudioRecorder();
-  
-    useEffect(() => {
-      if (!recordingBlob) return;
-  
-      // recordingBlob will be present at this point after 'stopRecording' has been called
-    }, [recordingBlob])
+import {IMediaRecorder, MediaRecorder, register} from 'extendable-media-recorder';
+import {connect} from 'extendable-media-recorder-wav-encoder';
+import { useState } from 'react';
+import Button from 'react-bootstrap/esm/Button';
 
-    return (
-      <div>
-        { !isRecording ?
-        <div>
-          <p>Press start to record the audio</p><Button onClick = {startRecording}>Start</Button>
-        </div> :
-        <div>
-          <p className="fade-in-fade-out">Recording...</p>
-          <Button variant='danger' onClick = {stopRecording}>Stop</Button>
-        </div> }
-      </div>
-    )
+let mediaRecorder: IMediaRecorder | null = null;
+let audioBlobs: BlobPart[] | undefined = [];
+let capturedStream: MediaStream | null = null;
+
+// Register the extendable-media-recorder-wav-encoder
+register(await connect());
+
+//async connect() {
+//  await register(await connect());
+//}
+
+// Starts recording audio
+function startRecording() {
+  return navigator.mediaDevices.getUserMedia({
+    audio: {
+      echoCancellation: true,
+    }
+  }).then(stream => {
+      audioBlobs = [];
+      capturedStream = stream;
+
+      // Use the extended MediaRecorder library
+      mediaRecorder = new MediaRecorder(stream, {
+        mimeType: 'audio/wav'
+      });
+
+      // Add audio blobs while recording 
+      mediaRecorder.addEventListener('dataavailable', event => {
+        if (audioBlobs != undefined)
+          audioBlobs.push(event.data);
+      });
+
+      mediaRecorder.start();
+  }).catch((e) => {
+    console.error(e);
+  });
+
+}
+
+function stopRecording() {
+  return new Promise<Blob | null>(resolve => {
+    if (!mediaRecorder) {
+      resolve(null);
+      return;
+    }
+
+    mediaRecorder.addEventListener('stop', () => {
+      let mimeType = ""
+
+      if (mediaRecorder)
+        mimeType = mediaRecorder.mimeType;
+
+      const audioBlob = new Blob(audioBlobs, { type: mimeType });
+
+      if (capturedStream) {
+        capturedStream.getTracks().forEach(track => track.stop());
+      }
+
+      resolve(audioBlob);
+    });
+    
+    mediaRecorder.stop();
+    
+  });
+}
+
+function playAudio(audioBlob: Blob | MediaSource) {
+  if (audioBlob) {
+    const audio = new Audio();
+    audio.src = URL.createObjectURL(audioBlob);
+    audio.play();
   }
+}
+
+export default function Recorder ()
+{
+  const [isRecording, setIsRecording] = useState(false)
+  function startRec()
+  {
+    startRecording();
+    setIsRecording(true);
+  }
+
+  async function stopRec()
+  {
+    //stopRecording();
+    const wavAudioBlob = await stopRecording();
+    stopRecording();
+    setIsRecording(false);
+    if(wavAudioBlob)
+      playAudio(wavAudioBlob);
+    
+  }
+
+  return (
+    <div>
+      { !isRecording ?
+      <div>
+        <p>Press start to record the audio</p><Button onClick = {startRec}>Start</Button>
+      </div> :
+      <div>
+        <p className="fade-in-fade-out">Recording...</p>
+        <Button variant='danger' onClick = {stopRec}>Stop</Button>
+      </div> }
+    </div>
+  )
+}
